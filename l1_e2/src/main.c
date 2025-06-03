8b6165c1183a22b705e3a1f55d1f28813e6ef45b
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2025-06-03 13:32:35 -0400

    polling version

--- l1_e2/src/main.c
+++ l1_e2/src/main.c
@@ -13,19 +13,25 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define SW0_NODE DT_ALIAS(sw0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec sw = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
 
 	if (!gpio_is_ready_dt(&led))
+	{
+		return 0;
+	}
+
+	if (!device_is_ready(&sw.port))
 	{
 		return 0;
 	}
@@ -36,16 +42,15 @@ int main(void)
 		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&sw, GPIO_INPUT);
+	if (ret < 0)
+	{
+		return 0;
+	}
+
 	while (1)
 	{
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0)
-		{
-			return 0;
-		}
-
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		gpio_pin_set_dt(&led, gpio_pin_get_dt(&sw));
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
